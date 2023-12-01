1b43511a26d7633c3a996224087f8d3e2dd8f352
root <root@LAPTOP-EVO5JPM5.localdomain>
2023-12-01 07:21:37 -0600

    cursor to game class

--- classes/game.cpp
+++ classes/game.cpp
@@ -1,4 +1,10 @@
 #include "game.hpp"
 #include "section.hpp"
 
-Game::Game() : state(GameState::START), drawSection(Section()), foundationSection(Section()), tableuSection(Section()) {}
+Game::Game() : state(GameState::START), drawSection(Section()), foundationSection(Section()), tableuSection(Section()),
+			   cursor1(new Cursor(&tableuSection)), cursor2(new Cursor(&tableuSection)) {}
+
+Game::~Game(){
+	delete cursor1;
+	delete cursor2;
+}
--- classes/game.hpp
+++ classes/game.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
 #include "section.hpp"
+#include "cursor.hpp"
 
 enum class GameState { START, ONGAME, WON };
 
 struct Game{
 	GameState state;
+	Cursor* cursor1;
+	Cursor* cursor2;
 	
 	Section drawSection;
 	Section foundationSection;
 	Section tableuSection;
 
 	Game();
+	~Game();
 };
